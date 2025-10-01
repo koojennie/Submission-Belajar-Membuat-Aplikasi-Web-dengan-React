8fd61ee7c041898846c46725280d145ca46ce6ed
jennie <aliyajenny14@gmail.com>
2025-10-01 10:05:12 +0700

    ediitng

--- src/components/NoteItemAction.jsx
+++ src/components/NoteItemAction.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function NoteItemAction({ id, archived, onDelete, onArchive }) {
   return (
     <div className="note-item__action">
-      <button className="note-item__delete-button" onClick={() => onDelete(id)}>Delete</button>
+      <button className="note-item__delete-button" onClick={() => onDelete(id)}>Hapus</button>
       <button className="note-item__archive-button" onClick={() => onArchive(id)}>
         {archived ? 'Pindahkan' : 'Arsipkan'}
       </button>
